3b9e6ab0dbfc260afb85ac743905e594223fa205
Ritik Shukla <shukla.ritik2001@gmail.com>
2024-03-09 13:13:05 +0530

    final concrete product

---
+++ src/components/Features.js
@@ -0,0 +1,322 @@
+import gridd from '../assets/grid.jpg';
+import darklit from '../assets/darklit.jpg';
+import seth from '../assets/Seth.jpg';
+import * as React from 'react';
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import Chip from '@mui/material/Chip';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import DevicesRoundedIcon from '@mui/icons-material/DevicesRounded';
+import EdgesensorHighRoundedIcon from '@mui/icons-material/EdgesensorHighRounded';
+import ViewQuiltRoundedIcon from '@mui/icons-material/ViewQuiltRounded';
+// import { Style } from '@mui/icons-material';
+import stratText from '../components/Strategy';
+// import stratText from '../components/strate'
+
+const items = [
+  {
+    icon: <ViewQuiltRoundedIcon />,
+    title: 'High Risk High Reward',
+    description:
+      'High risk, high potential returns. This strategy prioritizes investments that could yield significant gains but also carry a greater chance of substantial losses. Careful analysis and risk management are crucial.',
+    imageLight: 'url("/static/images/templates/templates-images/dash-light.png")',
+    imageDark: 'url("/static/images/templates/templates-images/dash-dark.png")',
+  },
+  {
+    icon: <EdgesensorHighRoundedIcon />,
+    title: 'Long Term Gain',
+    description:
+      'Like a snowball rolling downhill. Long-term gain trading is similar to a snowball rolling downhill. As your investments appreciate, they snowball in size, generating even greater returns over time due to compounding interest.',
+    imageLight: 'url("/static/images/templates/templates-images/mobile-light.png")',
+    imageDark: 'url("/static/images/templates/templates-images/mobile-dark.png")',
+  },
+  {
+    icon: <DevicesRoundedIcon />,
+    title: 'Safe Play',
+    description:
+      'Asset allocation for peace of mind. This strategy involves carefully allocating your investments across various asset classes based on your risk tolerance and financial goals. The aim is to create a balanced portfolio that weathers market fluctuations.',
+    imageLight: 'url("/static/images/templates/templates-images/devices-light.png")',
+    imageDark: 'url("/static/images/templates/templates-images/devices-dark.png")',
+  },
+];
+
+export default function Features() {
+  const [selectedItemIndex, setSelectedItemIndex] = React.useState(0);
+
+  const handleItemClick = (index) => {
+    // setTimeout(() => {}, 1500);
+    setSelectedItemIndex(index);
+    
+  };
+
+  const webexample = ["https://www.google.co.in", "https://www.youtube.com", "https://www.instagram.com"];
+
+  /* FadeIn Image Styling */
+
+  const fadeIn = (element, duration = 1000, display = 'block') => {
+    element.style.opacity = 0;
+    element.style.display = display;
+    let opacity = 0; 
+    const timer = setInterval(() => {
+      opacity += 50 / duration; // Increase smoothly based on duration
+      if (opacity >= 1) {
+        clearInterval(timer);
+        opacity = 1; // Ensure final full opacity
+      }
+      element.style.opacity = opacity;
+    }, 50); 
+  };
+
+  const [currentImage, setCurrentImage] = useState(`${gridd}`);  // Initial image
+  const images = [`${gridd}`, `${seth}`, `${darklit}`];
+  const handleImageChange = (img) => {
+    const imageElement = document.getElementById('image-box');
+    imageElement.style.opacity = 0; 
+    setCurrentImage(images[img]);
+    fadeIn(imageElement); 
+    // setCurrentImage(images[img]);
+  }
+
+  /* FadeIn Styling Ends */
+
+  const selectedFeature = items[selectedItemIndex];
+
+  return (
+    <Container id="features" sx={{ py: { xs: 8, sm: 16 } }}>
+      {/* <style>{mystyling}</style> */}
+      <Grid container spacing={6}>
+        <Grid item xs={12} md={6}>
+          <div>
+            <Typography component="h2" variant="h4" color="text.primary">
+              Product features
+            </Typography>
+            <Typography
+              variant="body1"
+              color="text.secondary"
+              sx={{ mb: { xs: 2, sm: 4 } }}
+            >
+              We offer the comprehensive solutions you need to make successful trades, from insightful data to automated strategies.
+              {/* Here you can provide a brief overview of the key features of the
+              product. For example, you could list the number of features, the types
+              of features, add-ons, or the benefits of the features. */}
+            </Typography>
+          </div>
+          <Grid container item gap={1} sx={{ display: { xs: 'auto', sm: 'none' } }}>
+            {items.map(({ title }, index) => (
+              <Chip
+                key={index}
+                label={title}
+                onClick={() => handleItemClick(index)}
+                sx={{
+                  borderColor: (theme) => {
+                    if (theme.palette.mode === 'light') {
+                      return selectedItemIndex === index ? 'primary.light' : '';
+                    }
+                    return selectedItemIndex === index ? 'primary.light' : '';
+                  },
+                  background: (theme) => {
+                    if (theme.palette.mode === 'light') {
+                      return selectedItemIndex === index ? 'none' : '';
+                    }
+                    return selectedItemIndex === index ? 'none' : '';
+                  },
+                  backgroundColor: selectedItemIndex === index ? 'primary.main' : '',
+                  '& .MuiChip-label': {
+                    color: selectedItemIndex === index ? '#fff' : '',
+                  },
+                }}
+              />
+            ))}
+          </Grid>
+          <Box
+            component={Card}
+            variant="outlined"
+            sx={{
+              display: { xs: 'auto', sm: 'none' },
+              mt: 4,
+            }}
+          >
+            <Box
+              sx={{
+                backgroundImage: (theme) =>
+                  theme.palette.mode === 'light'
+                    ? items[selectedItemIndex].imageLight
+                    : items[selectedItemIndex].imageDark,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                minHeight: 280,
+              }}
+            />
+            <Box sx={{ px: 2, pb: 2 }}>
+              <Typography color="text.primary" variant="body2" fontWeight="bold">
+                {selectedFeature.title}
+              </Typography>
+              <Typography color="text.secondary" variant="body2" sx={{ my: 0.5 }}>
+                {selectedFeature.description}
+              </Typography>
+              <Link
+                color="primary"
+                variant="body2"
+                fontWeight="bold"
+                onClick={(event) => {
+                  event.stopPropagation();
+                }}
+                
+                sx={{
+                  display: 'inline-flex',
+                  alignItems: 'center',
+                  '& > svg': { transition: '0.2s' },
+                  '&:hover > svg': { transform: 'translateX(2px)' },
+                }}
+              >
+                <span>Learn more</span>
+                <ChevronRightRoundedIcon
+                  fontSize="small"
+                  sx={{ mt: '1px', ml: '2px' }}
+                />
+              </Link>
+            </Box>
+          </Box>
+          <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="flex-start"
+            spacing={2}
+            useFlexGap
+            sx={{ width: '100%', display: { xs: 'none', sm: 'flex' } }}
+          >
+            {items.map(({ icon, title, description }, index) => (
+              <Card
+                key={index}
+                variant="outlined"
+                component={Button}
+                onClick={() => [handleItemClick(index), handleImageChange(index)]}
+                sx={{
+                  p: 3,
+                  height: 'fit-content',
+                  width: '100%',
+                  background: 'none',
+                  backgroundColor:
+                    selectedItemIndex === index ? 'action.selected' : undefined,
+                  borderColor: (theme) => {
+                    if (theme.palette.mode === 'light') {
+                      return selectedItemIndex === index
+                        ? 'primary.light'
+                        : 'grey.200';
+                    }
+                    return selectedItemIndex === index ? 'primary.dark' : 'grey.800';
+                  },
+                }}
+              >
+                <Box
+                  sx={{
+                    width: '100%',
+                    display: 'flex',
+                    textAlign: 'left',
+                    flexDirection: { xs: 'column', md: 'row' },
+                    alignItems: { md: 'center' },
+                    gap: 2.5,
+                  }}
+                >
+                  <Box
+                    sx={{
+                      color: (theme) => {
+                        if (theme.palette.mode === 'light') {
+                          return selectedItemIndex === index
+                            ? 'primary.main'
+                            : 'grey.300';
+                        }
+                        return selectedItemIndex === index
+                          ? 'primary.main'
+                          : 'grey.700';
+                      },
+                    }}
+                  >
+                    {icon}
+                  </Box>
+                  <Box sx={{ textTransform: 'none' }}>
+                    <Typography
+                      color="text.primary"
+                      variant="body2"
+                      fontWeight="bold"
+                    >
+                      {title}
+                    </Typography>
+                    <Typography
+                      color="text.secondary"
+                      variant="body2"
+                      sx={{ my: 0.5 }}
+                    >
+                      {description}
+                    </Typography>
+                    <Link
+                      color="primary"
+                      variant="body2"
+                      fontWeight="bold"
+                      href={webexample[index]} 
+                      component="a" 
+                      target="_blank" 
+                      rel="noopener noreferrer"
+                      sx={{
+                        display: 'inline-flex',
+                        alignItems: 'center',
+                        '& > svg': { transition: '0.2s' },
+                        '&:hover > svg': { transform: 'translateX(2px)' },
+                      }}
+                      onClick={() => {navigator.clipboard.writeText(stratText[index])}}
+                      // onClick={(event) => {
+                      //   event.stopPropagation();
+                      // }}
+                    >
+                      <span>Learn more</span>
+                      <ChevronRightRoundedIcon
+                        fontSize="small"
+                        sx={{ mt: '1px', ml: '2px' }}
+                      />
+                    </Link>
+                  </Box>
+                </Box>
+              </Card>
+            ))}
+          </Stack>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          sx={{ display: { xs: 'none', sm: 'flex' }, width: '100%' }}
+        >
+          <Card
+            variant="outlined"
+            sx={{
+              height: '100%',
+              width: '100%',
+              display: { xs: 'none', sm: 'flex' },
+              pointerEvents: 'none',
+            }}
+          >
+            <Box
+              onClick={() => {navigator.clipboard.writeText(stratText)}}
+              id = "image-box"
+              sx={{
+                m: 'auto',
+                width: 420,
+                height: 500,
+                backgroundSize: 'contain',
+                backgroundImage: `url(${currentImage})`,
+              }}
+            />
+          </Card>
+        </Grid>
+            <Button onClick={() => {navigator.clipboard.writeText(stratText)}}>Push Me!</Button>
+      </Grid>
+    </Container>
+  );
+}
---
+++ src/components/Strategy.js
@@ -0,0 +1,8 @@
+// This is a random text to be sent to clipboard of the user
+// function StratText () {
+//   var txt = "";
+// }
+
+const txt = ["This is a random text to be sent to clipboard of the user", "Text2", "Text3"];
+
+export default txt;
